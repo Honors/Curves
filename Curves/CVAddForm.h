6b319408465af582b3c07fe9494ad0ba8766aed5
Matt Neary <neary.matt@gmail.com>
2011-12-23 19:44:56 -0500

    first commit

---
+++ Curves/CVAddForm.h
@@ -0,0 +1,13 @@
+//
+//  CVAddForm.h
+//  Curves
+//
+//  Created by Matt Neary on 12/23/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CVAddForm : NSObject
+
+@end
---
+++ Curves/CVAddForm.m
@@ -0,0 +1,13 @@
+//
+//  CVAddForm.m
+//  Curves
+//
+//  Created by Matt Neary on 12/23/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import "CVAddForm.h"
+
+@implementation CVAddForm
+
+@end
