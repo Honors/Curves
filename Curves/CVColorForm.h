a193a362efc2da39745a477ec27356984a32a00b
Matt Neary <neary.matt@gmail.com>
2011-12-24 16:23:30 -0500

    Form sheet, Popover, DB structure

---
+++ Curves/CVColorForm.h
@@ -0,0 +1,13 @@
+//
+//  CVColorForm.h
+//  Curves
+//
+//  Created by Matt Neary on 12/24/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVColorForm : UIViewController
+
+@end
---
+++ Curves/CVColorForm.m
@@ -0,0 +1,13 @@
+//
+//  CVColorForm.m
+//  Curves
+//
+//  Created by Matt Neary on 12/24/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import "CVColorForm.h"
+
+@implementation CVColorForm
+
+@end
