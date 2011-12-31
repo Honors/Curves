d7f6a4f30de44b455a8955ecf35601ece63014bd
Matt Neary <neary.matt@gmail.com>
2011-12-31 00:18:33 -0500

    Version 1.0

---
+++ Curves/CVCustomKeyboardController.h
@@ -0,0 +1,13 @@
+//
+//  CVCustomKeyboardController.h
+//  Curves
+//
+//  Created by Matt Neary on 12/30/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CVCustomKeyboardController : UIViewController
+
+@end
---
+++ Curves/CVCustomKeyboardController.m
@@ -0,0 +1,13 @@
+//
+//  CVCustomKeyboardController.m
+//  Curves
+//
+//  Created by Matt Neary on 12/30/11.
+//  Copyright (c) 2011 OmniVerse. All rights reserved.
+//
+
+#import "CVCustomKeyboardController.h"
+
+@implementation CVCustomKeyboardController
+
+@end
